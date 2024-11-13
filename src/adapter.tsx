d405244856c6186034701336af512de85974b91f
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-11-14 03:07:00 +0900

    initialize

---
+++ src/adapter.tsx
@@ -0,0 +1,51 @@
+﻿"use client";
+
+import React, { createContext, useContext, useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { Chain, Account } from "@coinmeca/wallet-sdk/types";
+import { CoinmecaWalletAdapter } from "@coinmeca/wallet-sdk/adapter";
+
+interface CoinmecaWalletAdapterContextProps {
+    adapter: CoinmecaWalletAdapter | undefined;
+    account: Account | undefined;
+    // accounts: Account[] | undefined;
+    chain: Chain | undefined;
+    // chains: Chain[] | undefined;
+}
+
+const CoinmecaWalletAdapterContext = createContext<CoinmecaWalletAdapterContextProps | undefined>(undefined);
+
+export const useCoinmecaWallet = () => {
+    const context = useContext(CoinmecaWalletAdapterContext);
+    if (!context) throw new Error("InjectedWalletContext for useInjectedWallet doesn't initialized yet.");
+    return context;
+};
+
+export const CoinmecaWalletAdapterContextProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+    const [adapter, setAdapter] = useState<CoinmecaWalletAdapter>();
+
+    const [account, setAccount] = useState<Account>();
+    const [chain, setChain] = useState();
+
+    useLayoutEffect(() => {
+        setAdapter(new CoinmecaWalletAdapter());
+    }, []);
+
+    useEffect(() => {
+        const updateAccount = () => {
+            // setAccount(adapter?.account);
+        };
+
+        const updateChain = () => {
+            // setChain(adapter?.chain);
+        };
+
+        adapter?.on("accountChanged", updateAccount);
+        adapter?.on("chainChanged", updateChain);
+        return () => {
+            adapter?.off("accountChanged", updateAccount);
+            adapter?.off("chainChanged", updateChain);
+        };
+    }, [adapter]);
+
+    return <CoinmecaWalletAdapterContext.Provider value={{ adapter, account, chain }}>{children}</CoinmecaWalletAdapterContext.Provider>;
+};
---
+++ src/provider.tsx
@@ -0,0 +1,94 @@
+﻿"use client";
+
+import React, { createContext, useContext, useLayoutEffect, useState } from "react";
+import { Chain, Account, App } from "@coinmeca/wallet-sdk/types";
+import { CoinmecaWalletProvider } from "@coinmeca/wallet-sdk/provider";
+
+interface CoinmecaWalletProviderContextProps {
+    provider: CoinmecaWalletProvider | undefined;
+    account: Account | undefined;
+    accounts: Account[] | undefined;
+    chain: Chain | undefined;
+    chains: Chain[] | undefined;
+    apps: App[] | undefined;
+    tokens: {
+        fungibles?: string[];
+        nonFungibles?: string[];
+        multiTokens?: string[];
+    };
+}
+
+const CoinmecaWalletContext = createContext<CoinmecaWalletProviderContextProps | undefined>(undefined);
+
+export const useCoinmecaWalletProvider = () => {
+    const context = useContext(CoinmecaWalletContext);
+    if (!context) throw new Error("InjectedWalletContext for useInjectedWallet doesn't initialized yet.");
+    return context;
+};
+
+export const CoinmecaWalletContextProvider: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
+    const [provider, setProvider] = useState<CoinmecaWalletProvider>();
+
+    const [account, setAccount] = useState<Account>();
+    const [chain, setChain] = useState<Chain>();
+    const [fungibles, setFungibles] = useState<any>();
+
+    useLayoutEffect(() => {
+        const chainId = (window as any)?.coinmeca?.request?.chainId;
+        const provider = (window as any)?.coinmeca?.wallet || new CoinmecaWalletProvider({ chainId });
+        setProvider(provider);
+    }, []);
+
+    useLayoutEffect(() => {
+        if (provider) {
+            const updateAccount = () => {
+                setAccount(provider?.account());
+            };
+
+            const updateChain = () => {
+                setChain(provider?.chain);
+            };
+
+            const updateApps = () => {};
+
+            const updateFungibles = () => {
+                setAccount(provider?.account());
+            };
+
+            const update = () => {
+                updateAccount();
+                updateChain();
+            };
+
+            provider?.on("unlock", update);
+            provider?.on("accountChanged", updateAccount);
+            provider?.on("chainChanged", updateChain);
+            provider?.on("updateFungibleAsset", updateFungibles);
+            return () => {
+                provider?.off("unlock", update);
+                provider?.off("accountChanged", updateAccount);
+                provider?.off("chainChanged", updateChain);
+                provider?.off("updateFungibleAsset", updateFungibles);
+            };
+        }
+    }, [provider]);
+
+    return (
+        <CoinmecaWalletContext.Provider
+            value={{
+                provider,
+                account,
+                chain,
+                accounts: provider?.accounts() as Account[],
+                chains: provider?.chains,
+                apps: provider?.apps,
+                tokens: {
+                    fungibles: chain?.chainId ? account?.tokens?.fungibles?.[`${chain?.chainId}`] : undefined,
+                    nonFungibles: chain?.chainId ? account?.tokens?.nonFungibles?.[`${chain?.chainId}`] : undefined,
+                    multiTokens: chain?.chainId ? account?.tokens?.multiTokens?.[`${chain?.chainId}`] : undefined,
+                },
+            }}>
+            {children}
+        </CoinmecaWalletContext.Provider>
+    );
+};
