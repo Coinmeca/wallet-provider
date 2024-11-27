261463cc68ea4a85d3722ec69f2d4f240c6dbc85
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-11-28 05:38:34 +0900

    update sdk

---
+++ next.config.ts
@@ -0,0 +1,59 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/chainlist/:path*', // Path after '/api/chainlist'
+        destination: 'https://chainlist.org/api/:path*', // Forward to chainlist API,
+      }
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,OPTIONS" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, Content-Type, Access-Control-Allow-Headers, DeviceInfo, Authorization, X-Requested-With",
+          },
+        ],
+      },
+    ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  i18n: {
+    locales: ["en", "es", "fr", "cn", "jp", "id", "vi"],
+    defaultLocale: "en",
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "web3.coinmeca.net",
+      },
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  experimental: {
+    // ppr: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+export default nextConfig;
